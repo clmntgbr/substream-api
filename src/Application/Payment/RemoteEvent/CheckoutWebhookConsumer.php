b42c239b704183c0cc0de46aa6daef24a20d2379
Clément <33451311+clmntgbr@users.noreply.github.com>
2025-11-14 18:34:42 +0100

    Enhance CheckoutCompletedCommand and CheckoutWebhookConsumer for improved validation and structure
    - Updated CheckoutCompletedCommand to include validation constraints using Symfony's Validator component, ensuring required fields are not blank.
    - Added exception handling in getter methods of CheckoutCompletedCommand to throw errors when required fields are null, improving robustness.
    - Refactored CheckoutWebhookConsumer to utilize a StripeObject for better clarity and maintainability when dispatching the CheckoutCompletedCommand.
    
    These changes enhance the integrity of the checkout process and improve the overall structure of command and event handling in the application.

--- src/Application/Payment/RemoteEvent/CheckoutWebhookConsumer.php
+++ src/Application/Payment/RemoteEvent/CheckoutWebhookConsumer.php
@@ -5,6 +5,7 @@ namespace App\Application\Payment\RemoteEvent;
 use App\Application\Payment\Command\CheckoutCompletedCommand;
 use App\Shared\Application\Bus\CommandBusInterface;
 use Stripe\Event;
+use Stripe\StripeObject;
 use Symfony\Component\RemoteEvent\Attribute\AsRemoteEventConsumer;
 use Symfony\Component\RemoteEvent\Consumer\ConsumerInterface;
 use Symfony\Component\RemoteEvent\RemoteEvent;
@@ -22,17 +23,20 @@ final class CheckoutWebhookConsumer implements ConsumerInterface
         /** @var Event $payload */
         $payload = $event->getPayload()['payload'];
 
+        /** @var StripeObject $stripeObject */
+        $stripeObject = $payload->data->object;
+
         $this->commandBus->dispatch(new CheckoutCompletedCommand(
-            checkoutSessionId: $payload->data->object->id,
-            userId: $payload->data->object->client_reference_id,
-            userEmail: $payload->data->object->customer_email,
-            planId: $payload->data->object->metadata->plan_id,
-            stripeCustomerId: $payload->data->object->customer,
-            subscriptionId: $payload->data->object->subscription,
-            stripeInvoiceId: $payload->data->object->invoice,
-            paymentStatus: $payload->data->object->payment_status,
-            amount: $payload->data->object->amount_total,
-            currency: $payload->data->object->currency,
+            checkoutSessionId: $stripeObject->id,
+            userId: $stripeObject->client_reference_id,
+            userEmail: $stripeObject->customer_email,
+            planId: $stripeObject->metadata->plan_id,
+            stripeCustomerId: $stripeObject->customer,
+            subscriptionId: $stripeObject->subscription,
+            stripeInvoiceId: $stripeObject->invoice,
+            paymentStatus: $stripeObject->payment_status,
+            amount: $stripeObject->amount_total,
+            currency: $stripeObject->currency,
         ));
     }
 }
